1a40e32ceeeab92c06e42ab8ee4bace031895a28
ohkee <csohkee@gmail.com>
2024-06-10 20:06:39 -0500

    - made the gui functional to the script

--- gui.py
+++ gui.py
@@ -1,66 +1,151 @@
 from tkinter import *
+from tkinter.ttk import Combobox
+
 import customtkinter
+import psutil
+import viewbot
+import threading
 
 # Configuration
 THEME = 'light'
+RES = '450x300'
 
-def handle_enter(event):
-    text = entry_text.get()
-    print("Entered text:", text)
-    entry.configure(state="disabled")
+# Globals
+viewbot_running = False
+viewbot_thread = None
+stop_flag = threading.Event()
 
-if __name__ == '__main__':
-    # Creates app
-    customtkinter.set_appearance_mode(THEME)
-    app = customtkinter.CTk()
-    app.geometry('800x600')
-    app.title('VIEWBOT')
+def update_cpu_label():
+    global cpu_percent_label
+    cpu_percent = psutil.cpu_percent()
+    cpu_percent_label.config(text=f'CPU: {cpu_percent}%')
+    # Schedule the function to run again after 1000 milliseconds (1 second)
+    cpu_percent_label.after(1000, update_cpu_label)
 
-    # Method
-    method_frame = customtkinter.CTkFrame(master=app)
-    method_frame.grid(row=1, column=0, padx=(0,50), pady=(20, 10), sticky="nsew")
-    method_frame.grid_columnconfigure(0, weight=1)  # make column 0 expandable
+def update_ram_label():
+    global ram_percent_label
+    ram_percent = psutil.virtual_memory().percent
+    ram_percent_label.config(text=f'RAM: {ram_percent}%')
+    # Schedule the function to run again after 1000 milliseconds (1 second)
+    ram_percent_label.after(1000, update_ram_label)
 
-    method_label = customtkinter.CTkLabel(master=method_frame, text="Method")
+def increment():
+    current_value = int(number_of_viewers.get())
+    number_of_viewers.set(current_value + 1)
+
+def decrement():
+    current_value = int(number_of_viewers.get())
+    if current_value > 0:
+        number_of_viewers.set(current_value - 1)
+
+def on_mousewheel(event):
+    if event.delta > 0 or event.num == 4:
+        increment()
+    elif event.delta < 0 or event.num == 5:
+        decrement()
+
+def create_method_frame(master):
+    method_frame = Frame(master=master)
+    method_frame.grid(row=1, column=0, padx=10, pady=10, sticky="nsew")
+
+    method_label = Label(master=method_frame, text="Method")
     method_label.grid(row=0, column=0, padx=10, pady=0, sticky="w")
 
     method_label_var = StringVar()
     method_label_var.set('web proxy')
-    method_box = customtkinter.CTkComboBox(master=method_frame, values=['web proxy', 'premium proxy (must have proxies)'], variable=method_label_var)
-    method_box.grid(row=1, column=0, padx=10, pady=(10 , 10), sticky="ew")
+    method_box = Combobox(master=method_frame, values=['web proxy', 'personal proxy'], textvariable=method_label_var, state='disabled', )
+    method_box.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
+    return method_frame, method_label_var
 
-    # Platform
-    platform_frame = customtkinter.CTkFrame(master=app)
-    platform_frame.grid(row=0, column=0, padx=(0,50), pady=(20, 10), sticky="nsew")
-    platform_frame.grid_columnconfigure(0, weight=1)  # make column 0 expandable
+def create_platform_frame(master):
+    platform_frame = Frame(master=master,highlightthickness=1, highlightbackground="black", bd=1, height=100, width=500)
+    platform_frame.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-    platform_label = customtkinter.CTkLabel(master=platform_frame, text="Platform")
+    platform_label = Label(master=platform_frame, text="Platform")
     platform_label.grid(row=0, column=0, padx=10, pady=0, sticky="w")
 
-    platform_box_var = StringVar()
-    platform_box_var.set('twitch')
-    platform_box = customtkinter.CTkComboBox(master=platform_frame, values=['twitch', 'kick'], variable=platform_box_var)
-    platform_box.grid(row=1, column=0, padx=10, pady=(10 , 10), sticky="ew")
+    platform_box = Combobox(master=platform_frame, values=['twitch', 'kick'], textvariable=platform)
+    platform_box.grid(row=1, column=0, padx=10, pady=10, sticky="ew")
 
-    # Channel 
-    channel_label = customtkinter.CTkLabel(master=platform_frame, text="Channel Name")
+    # CPU / RAM
+    global cpu_percent_label 
+    cpu_percent_label = Label(platform_frame, text='CPU: -')
+    cpu_percent_label.grid(row=1, column=1, padx=50, pady=10)
+    update_cpu_label() # Start updating the CPU label
+
+    global ram_percent_label 
+    ram_percent_label = Label(platform_frame, text='RAM: -')
+    ram_percent_label.grid(row=2, column=1, padx=50, pady=10)
+    update_ram_label() # Start updating the RAM label
+    return platform_frame, platform
+
+def create_channel_entry(platform_frame):
+    channel_label = Label(master=platform_frame, text="Channel Name")
     channel_label.grid(row=2, column=0, padx=10, pady=10, sticky="w")
-    channel_entry = customtkinter.CTkEntry(master=platform_frame)
-    channel_entry.grid(row=2, column=0, padx=10, pady=(0, 10), sticky="ew")
+    channel_entry = Entry(master=platform_frame, textvariable=channel_name)
+    channel_entry.grid(row=3, column=0, padx=10, pady=(0, 10), sticky="ew")
 
-    # Entry for viewers
-    viewers_label = customtkinter.CTkLabel(master=platform_frame, text="Number of Viewers")
-    viewers_label.grid(row=3, column=0, padx=10, pady=10, sticky="w")
-    viewers_entry = customtkinter.CTkEntry(master=platform_frame)
-    viewers_entry.grid(row=4, column=0, padx=10, pady=(0, 10), sticky="ew")
+def create_viewers_entry(platform_frame):
+    viewers_label = Label(master=platform_frame, text="Viewers:")
+    viewers_label.grid(row=4, column=0, padx=8, pady=10, sticky="w")
 
-    # Start and Stop buttons
-    start_button = customtkinter.CTkButton(master=platform_frame, text="Start", width=60,height=20, corner_radius=0, fg_color="#4CAF50", hover_color="#45A049", text_color="white")
-    start_button.grid(row=4, column=1, padx=10, pady=(5, 10))
+    viewers_entry = Spinbox(platform_frame, textvariable=number_of_viewers, from_=0, to=100)
+    viewers_entry.grid(row=4, column=0, padx=60, sticky='e')
 
-    stop_button = customtkinter.CTkButton(master=platform_frame, text="Stop", width=60, height=20, corner_radius=0, fg_color="#F44336", hover_color="#D32F2F", text_color="white")
-    stop_button.grid(row=4, column=2, padx=0, pady=(5, 10))
+    # Bind mouse wheel events
+    viewers_entry.bind("<MouseWheel>", lambda event: on_mousewheel(event)) 
+
+def start_viewbot():
+    global viewbot_thread
+    stop_flag.clear()
+    platform_value = platform.get()
+    channel_value = channel_name.get()
+    viewers_value = number_of_viewers.get()
+    viewbot_thread = threading.Thread(target=viewbot.start_viewbot, args=(stop_flag, platform_value, channel_value, viewers_value))
+    viewbot_thread.start()
+
+def stop_viewbot():
+    global viewbot_thread, stop_flag
+    if viewbot_thread is not None and viewbot_thread.is_alive():
+        stop_flag.set()
+        viewbot_thread.join()
+
+def toggle_viewbot(button):
+    if viewbot_thread is None or not viewbot_thread.is_alive():
+        button.config(text='Stop')
+        start_viewbot()
+    else:
+        button.config(text='Start')
+        stop_viewbot()
+
+def create_control_buttons(platform_frame):
+    start_button = Button(master=platform_frame, text="Start", width=10, command=lambda: toggle_viewbot(start_button))
+    start_button.grid(row=4, column=1, padx=20, pady=(0, 20))
+
+def main():
+    customtkinter.set_appearance_mode(THEME)
+    app = customtkinter.CTk()
+    app.geometry(RES)
+    app.resizable(False, False)
+    app.iconbitmap('icon.ico')
+    app.title('Viewbot')
+
+    # Initialize Tkinter variables
+    global platform, method, channel_name, number_of_viewers
+    platform = StringVar(value='twitch')
+    method = StringVar(value='web proxy')
+    channel_name = StringVar(value='')
+    number_of_viewers = IntVar(value=0)
 
 
-    # Runs the app
+    method_frame, method_box_var = create_method_frame(app) 
+    platform_frame, platform_box_var = create_platform_frame(app)
+
+    create_channel_entry(platform_frame)
+    create_viewers_entry(platform_frame)  
+    create_control_buttons(platform_frame)
+
     app.mainloop()
+
+if __name__ == '__main__':
+    main()
--- viewbot.py
+++ viewbot.py
@@ -1,16 +1,11 @@
-import time
-import random
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 
-# Configuration
-PLATFORM = 'twitch.tv/'
-PLATFORM_NAME = PLATFORM +'yaga4'
-NUMBER_OF_VIEWERS = 60
+import random
+import time
 
 def create_driver():
     options = Options()
@@ -31,25 +26,22 @@ def randomize_server():
     servers = ['https://www.croxyproxy.com/','https://proxyium.com/', 'https://www.croxy.org/', 'https://www.blockaway.net/']
     return random.choice(servers)
 
-if __name__ == "__main__":
-
+def start_viewbot(stop_flag, platform, channel_name, number_of_viewers):
     driver = create_driver()
+    viewbot_running = False
 
-    for x in range(NUMBER_OF_VIEWERS):
-        driver.switch_to.new_window('window')
-        driver.get(randomize_server())
-        driver.implicitly_wait(2)
+    while not stop_flag.is_set():
+        if not viewbot_running:
+            viewbot_running = True
 
-        input_box = driver.find_element(by=By.NAME, value='url')
-        input_box.send_keys(TWITCH_NAME)
-        input_box.send_keys(Keys.RETURN)
-        driver.implicitly_wait(2)
+            for _ in range(number_of_viewers):
+                if stop_flag.is_set():  
+                    break
+                driver.switch_to.new_window('window')
+                driver.get(randomize_server())
+                driver.implicitly_wait(2)
 
-    # Keep the script running to keep all browser windows open
-    try:
-        while True:
-            time.sleep(60)    
-    except KeyboardInterrupt:
-       # Close all drivers on exit
-        print("All browser windows have been closed.")
-        driver.quit()
+                input_box = driver.find_element(by=By.NAME, value='url')
+                input_box.send_keys(f'{platform}.tv/{channel_name}')
+                input_box.send_keys(Keys.RETURN)
+                driver.implicitly_wait(2)
